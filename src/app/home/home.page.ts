9979a9e362381a41a6459a160c7240f77645c224
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-05-29 16:33:34 -0400

    added home page and service logic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { EventResponse } from '../interfaces';
+import { Subscription } from 'rxjs';
+import { EventsService } from '../services/events/events.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  private events: EventResponse[] = [];
+  public sub: Subscription;
 
-  constructor() {}
+  constructor(
+    private _eventsService: EventsService,
+    private _nav: NavController
+  ) { }
+
+  ngOnInit() {
+    this.sub = this._eventsService.getAll().subscribe((e: EventResponse) => {
+      this.events.push(e);
+    })
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  public getEvents(): EventResponse[] {
+    return this.events.sort((a: EventResponse, b: EventResponse) => a.event.created > b.event.created ? -1 : 1 );
+  }
+
+  public details(response: EventResponse) {
+    this._nav.navigateForward(`details/${response.event.id}`)
+  }
 
 }
--- src/app/services/events/events.service.ts
+++ src/app/services/events/events.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { EventResponse, Acknowledgement } from './../../interfaces';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
+  private endpoint = 'https://us-central1-ps-notify-api.cloudfunctions.net/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<EventResponse> {
+    return Observable.create((observer: Observer<EventResponse>) => {
+      const self = this;
+      self.getLatest().subscribe(res => onNext(res), observer.error);
+
+      function onNext(response: EventResponse) {
+        observer.next(response);
+        if (response.links.next) {
+        self.getByRoute<EventResponse>(response.links.next).subscribe(res => onNext(res), observer.error);
+        } else {
+          observer.complete();
+        }
+      }
+    });
+  }
+
+  getLatest(): Observable<EventResponse> {
+    const route = `/latest`;
+    return this.getByRoute(route);
+  }
+
+  getById(id: number): Observable<EventResponse> {
+    const route = `/event/${id}`;
+    return this.getByRoute(route);
+  }
+
+  private getByRoute<T>(route: string): Observable<T> {
+    const url = `${this.endpoint}${route}`;
+    return this.http.get<T>(url);
+  }
+
+  getNewestEventId(): number {
+    // Higher numbers are newer
+    // For now, let's return 10.
+    // The REST service returns random events, from n -> 0.
+    return 10;
+
+    // Eventually, we'll want to store this value locally.
+  }
+
+  getAcknowledgements(event: EventResponse) {
+    return this.getByRoute<Acknowledgement[]>(event.links.acknowledgements);
+  }
 }
